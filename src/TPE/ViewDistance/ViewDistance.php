ccfa85681a2fd9cec3016af0c0a4a2e74fd13fa9
TPE <60147636+TPEimperialPE@users.noreply.github.com>
2020-09-25 21:48:42 +0100

    Add files via upload

---
+++ src/TPE/ViewDistance/ViewDistance.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TPE\ViewDistance;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+
+class ViewDistance extends PluginBase {
+
+    public function onEnable() {
+        $this->saveDefaultConfig();
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if(!$sender instanceof Player) {
+            return true;
+        }
+        if(!is_numeric($args[0])) {
+            $sender->sendMessage($this->getConfig()->get("not_numeric"));
+            return true;
+        }
+        if(!$sender->hasPermission("view.command")) {
+            $sender->sendMessage($this->getConfig()->get("no_perms_message"));
+        }
+        if($command->getName() === "view") {
+            if(isset($args[0])) {
+                $distance = $sender->getViewDistance();
+                $sender->setViewDistance($args[0]);
+                $config = str_replace("%distance%", $distance, $this->getConfig()->get("view_change_success"));
+                $sender->sendMessage($this->getConfig()->get("view_change_success"));
+            }
+        }
+    }
+
+}
